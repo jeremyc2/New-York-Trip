72e92677a5a41ac413cbb8129574ed4a77965988
Jeremy Chandler <goldenmeso@gmail.com>
2020-09-18 16:51:44 -0500

    empty

--- serviceworker.js
+++ serviceworker.js
@@ -4,8 +4,27 @@ const path = (new URL(self.registration.scope)).pathname;
 
 var cacheName = "NYC-V1.0";
 const cachefiles = [
-    
-];
+    path, 
+    path + "cost", 
+    path + "images/boringstuff3.jpg", 
+    path + "images/down-arrow.svg", 
+    path + "images/icons8-new-york-40.png", 
+    path + "images/photodate.jpg", 
+    path + "images/screen_2x.jpg", 
+    path + "images/sightseeing-unsplash%20compressed.jpg", 
+    path + "images/william-wachter-HLNLtyon-xg-unsplash%20compressed.jpg", 
+    path + "scripts/app.js", 
+    path + "styles/keyframes.css", 
+    path + "styles/main-mobile.css", 
+    path + "styles/main.css", 
+    path + "styles/secondSlide.css", 
+    "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js", 
+    "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js", 
+    "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js", 
+    "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js", 
+    "https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap", 
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/CSSRulePlugin3.min.js"
+]
 
 self.addEventListener("install", event => {
     console.log("Installing...");
