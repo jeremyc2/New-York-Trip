c9cee94e59d1b27705169f9cf3f18fa07d5175c6
Jeremy Chandler <goldenmeso@gmail.com>
2020-09-19 14:21:03 -0500

    cleanup

--- serviceworker.js
+++ serviceworker.js
@@ -2,21 +2,34 @@ const path = (new URL(self.registration.scope)).pathname;
 
 importScripts(path + "scripts/jsonUtils.js");
 
+var a = {};
+
 var cacheName = "NYC-V1.0";
 const cachefiles = [
-    path, 
-    path + "images/boringstuff3.jpg", 
-    path + "images/down-arrow.svg", 
-    path + "images/icons8-new-york-40.png", 
-    path + "images/photodate.jpg", 
-    path + "images/screen_2x.jpg", 
-    path + "images/sightseeing-unsplash%20compressed.jpg", 
-    path + "images/william-wachter-HLNLtyon-xg-unsplash%20compressed.jpg", 
-    path + "scripts/app.js", 
-    path + "styles/keyframes.css", 
-    path + "styles/main-mobile.css", 
-    path + "styles/main.css", 
-    path + "styles/secondSlide.css"
+    path,
+    path + "activities/sightseeing",
+    path + "activities/sightseeing.html",
+    path + "fonts/montserrat-latin.woff2",
+    path + "images/boringstuff3.jpg",
+    path + "images/down-arrow.svg",
+    path + "images/icons8-new-york-40.png",
+    path + "images/nyc-vectorstock.svg",
+    path + "images/photodate.jpg",
+    path + "images/screen_2x.jpg",
+    path + "images/sightseeing-unsplash%20compressed.jpg",
+    path + "images/william-wachter-HLNLtyon-xg-unsplash%20compressed.jpg",
+    path + "scripts/CSSRulePlugin3.min.js",
+    path + "scripts/ScrollMagic.min.js",
+    path + "scripts/animation.gsap.min.js",
+    path + "scripts/app.js",
+    path + "scripts/debug.addIndicators.min.js",
+    path + "scripts/gsap.min.js",
+    path + "styles/keyframes.css",
+    path + "styles/main-mobile.css",
+    path + "styles/main.css",
+    path + "styles/montserrat.css",
+    path + "styles/secondSlide.css",
+    path + "styles/sightseeing.css"
 ]
 
 self.addEventListener("install", event => {
@@ -41,6 +54,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     const parsedUrl = new URL(event.request.url);
+
+    a[event.request.url] = true;
 
     // navigator.onLine
     // might have to clone request and response
